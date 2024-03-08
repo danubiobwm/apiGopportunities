0302a68ac0018edf731d004acda3abfe4ef3df53
Danubio de Araujo <danubio.bwm@gmail.com>
2024-03-07 22:39:43 -0300

    feat: add separate files for route handlers

---
+++ handler/DeleteOpeningHandler.go
@@ -0,0 +1,11 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+
+
+func DeleteOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{
+		"message": "Delete opening",
+	})
+}
---
+++ handler/ListOpeningsHandler.go
@@ -0,0 +1,12 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+
+
+
+func ListOpeningsHandler(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{
+		"message": "Get all openings",
+	})
+}
