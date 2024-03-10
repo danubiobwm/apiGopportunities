f9848eafa29331f21e377d50efb20ba457be93f8
danubiobwm <danubio.bwm@gmail.com>
2024-03-10 17:59:45 -0300

    loggers

--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "github.com/danubiobwm/apiGopportunities/router"
+import (
+	"github.com/danubiobwm/apiGopportunities/config"
+	"github.com/danubiobwm/apiGopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("Main")
+
+	// Initialize Config
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("Error initializing config: %s", err)
+		return
+	}
+
 	// func Initialize
 	router.Initialize()
+
 }
