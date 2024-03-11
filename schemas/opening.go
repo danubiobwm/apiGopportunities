44d9cbb5e86f4b4cead518cc2c6b8423b7d877b8
danubiobwm <danubio.bwm@gmail.com>
2024-03-10 21:16:51 -0300

    opening response

--- schemas/opening.go
+++ schemas/opening.go
@@ -1,16 +1,30 @@
 package schemas
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Opening struct {
 	gorm.Model
-	Role string `json:"role"`
-	Company string `json:"company"`
+	Role     string `json:"role"`
+	Company  string `json:"company"`
 	Location string `json:"location"`
-	Remote bool  `json:"remote"`
-	Link string `json:"link"`
-	Salary int64 `json:"salary"`
-
+	Remote   bool   `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
 }
 
-
+type OpeningResponse struct {
+	ID        uint      `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
+	Role      string    `json:"role"`
+	Company   string    `json:"company"`
+	Location  string    `json:"location"`
+	Remote    bool      `json:"remote"`
+	Link      string    `json:"link"`
+	Salary    int64     `json:"salary"`
+}
